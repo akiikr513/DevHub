d8ecc6f260a5cc58f722758782e02be6fa4e2865
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-01-20 15:37:34 +0530

    Register User in users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-//@route GET api/users public access
-router.get('/', (req, res) => res.send('User Route'));
+//@route Post api/users public access
+//@desc Register users
+router.post(
+  '/',
+  [
+    check('name', 'Name is Required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a Valid Email').isEmail(),
+    check('password', 'Please enter a password min length is 6').isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('User Route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 connectDB();
+
+//initilizing middleware
+app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API CHAL RAHA H  Running'));
 
 // Define Routes
