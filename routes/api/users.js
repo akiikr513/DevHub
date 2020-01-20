d0cabfb8cfbd624ef558458f7c98115a253a6a59
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-01-20 16:13:56 +0530

    used mangoose to post data to mangoDB atlas

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
 const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 //@route Post api/users public access
 //@desc Register users
@@ -15,13 +20,43 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('User Route');
+    const { name, email, password } = req.body;
+
+    try {
+      // check user is exists -done-, get user gravatar -done-,encrypt pass -done- and retur jsonwebtoken
+
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User Already Exists' }] });
+      }
+
+      const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User Registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server started on Port ${PORT}'));
+app.listen(PORT, () => console.log('Server started on Port: ${PORT} '));
