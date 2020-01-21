030321dcc1c121087f08c12d389823fda2d1fc3b
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-01-22 02:01:51 +0530

    Adding and Deleting Experience in Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -142,4 +142,74 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+//@route PUT api/profile/experience @desc add Profile experience, private access
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is Required.')
+        .not()
+        .isEmpty(),
+      check('company', 'Company is Required.')
+        .not()
+        .isEmpty(),
+      check('from', 'From Date is Required.')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+//@route Delete api/profile/experience  @desc delete Profile experience, private access
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
