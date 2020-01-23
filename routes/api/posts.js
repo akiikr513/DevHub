c8ec978ee41f289fb2ce1376426acd4b603884b6
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-01-23 06:49:22 +0530

    added Like And Unlike route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -93,4 +93,49 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+//@route PUT api/posts/:id, Desc like a posts, private access
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //check for liked or not.
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: 'Post Already Liked' });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+//@route PUT api/posts/:id, Desc unlike a posts, private access
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //check for liked or not.
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: 'First like then try' });
+    }
+
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
