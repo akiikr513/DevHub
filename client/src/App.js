5b4177ded43bfddfde65bb2de45e6c9b36ccfd21
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-01-29 04:39:13 +0530

    added prfile and post with comments/likes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,10 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import Routes from './components/routing/Routes';
 import './App.css';
 //redux
 import { Provider } from 'react-redux';
-import Alert from './components/layout/Alert';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
@@ -26,14 +24,10 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
