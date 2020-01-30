6eedd785df73f0c314fd1e1bc24a6a10e33daca7
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-01-30 22:48:46 +0530

    aaded all last commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
+
+//connect DB
 connectDB();
 
 //initilizing middleware
 app.use(express.json({ extended: false }));
-app.get('/', (req, res) => res.send('API CHAL RAHA H  Running'));
 
 // Define Routes
 
@@ -13,6 +15,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+//static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server started on Port: ${PORT} '));
